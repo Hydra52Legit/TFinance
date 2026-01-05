70723cabfef797edb886705d8c87a6dae2ef2e31
Hydra <hydrawork777@gmail.com>
2026-01-05 15:48:34 +0500

    Initial Commit

---
+++ TFinance/TFinanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TFinanceApp.swift
+//  TFinance
+//
+//  Created by Alex Kornilov on 5. 1. 2026..
+//
+
+import SwiftUI
+
+@main
+struct TFinanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
